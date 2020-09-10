f5cc51d2305c69c2bb95d2a515faba4f03a6e030
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-09 21:32:13 -0700

    finish Password generator loop

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
 console.log("Hi there!")
 
-document.getElementById("passGenerator")
-
+// make this accept an argument
 function passGen() {
     alert("Button Works!");
 };
+
+var charsAllowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@%!$";
+var passLen = 10;
+var userPass = "";
+
+
+for (var i = 0; i < passLen; i++) {
+   userPass += charsAllowed.charAt(Math.floor(Math.random() * charsAllowed.length));
+}
+console.log(userPass);
