e0176989f09c3789b1f7e9d256bbc5b0cde25f86
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-09 22:23:31 -0700

    refactor passGen and prep for HTML changes

--- javascript.js
+++ javascript.js
@@ -1,24 +1,74 @@
 console.log("Hi there!")
 
 
-var charsAllowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@%!$";
-
+var charsAllowed = "";
+// alt var charsAllowed = includeCriteria();
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
 var userPass = "";
-// make this accept an argument
+
+
+
+
+
+
+
+
+
 function passGen() {
-    var passLen = prompt("Please Enter the Desired Password Length");
+    var passLen = prompt("Please Enter the Desired Password Length in Numbers between 8 and 128");
+
+    // ask User for Upper,Lower,Number, and Symbols in password
+
+    // clear textarea of existing text(if any)
     passClear();
+    
+    
+    
+    
+    
+    
     for (var i = 0; i < passLen; i++) {
         userPass += charsAllowed.charAt(Math.floor(Math.random() * charsAllowed.length));
      }
      document.getElementById("genPassField").value = userPass;
-};
+
+
+
+
+
+
+
+    };
+
+
+    function includeCriteria() {
+        var includeUpper = confirm("Include Uppercase characters?");
+        if (includeUpper === true) {
+            charsAllowed 
+        }
+        var includeLower = confirm("Include Lowercase characters?");
+        var includeNumbers = confirm("Include Number characters?");
+        var includeSpecial = confirm("Include Special characters?");
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function passClear() {
     userPass= "";
     console.log("working?")
 };
-
-
-
-
