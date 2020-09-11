c3587d2f415b9e76b251e11b1c7560f635f2fcf1
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-11 09:52:38 -0700

    added comments in code for readability
    '

--- javascript.js
+++ javascript.js
@@ -3,31 +3,25 @@ var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var numbers = '0123456789';
 var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
 var userPass = "";
-// function
+// Called by button "onclick" attributte. Generates a randomized password from a user generated list of characters.
 function passGen() {
     var passLen = document.getElementById("numUserChar").innerHTML;
-console.log(passLen)
     // Generate user Character Set to use in password generation
     var charsAllowed = includeCriteria();
-    console.log(charsAllowed)
     // clear textarea of existing text(if any)
     passClear();
     // Generate the password
     for (var i = 0; i < passLen; i++) {
         userPass += charsAllowed.charAt(Math.floor(Math.random() * charsAllowed.length));
      }
+     //prints Generated Password to Screen. 
      document.getElementById("genPassField").value = userPass;
 };
-
-
+// builds character set to use when randomizing password and validates that user has selected at least one input. 
 function includeCriteria() {
-
-    var charsRequested = "";
-
-    
+    var charsRequested = "";    
     if (document.getElementById("upperCase").checked === false && document.getElementById("lowerCase").checked === false && document.getElementById("nums").checked === false && document.getElementById("specChar").checked === false) {
         alert("You must choose at least one character set!");
-        
     } else {
         if (document.getElementById("upperCase").checked) {
             charsRequested += uppercase;
@@ -42,13 +36,11 @@ function includeCriteria() {
             charsRequested += symbols;
         }
         return charsRequested;
-    }
-    
+    }   
 };
-
+// Clears Text Area. Called by passGen();
 function passClear() {
     userPass = "";
-    console.log("working?")
 };
 //slider code largely copied from https://www.w3schools.com/howto/howto_js_rangeslider.asp
 var slider = document.getElementById("rangeSlide");
